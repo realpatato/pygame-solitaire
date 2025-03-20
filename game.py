f015631033eec2169db7fc6f79eac7b92cbce2c6
Jason <jason.t.richardii@gmail.com>
2025-03-20 18:38:44 -0400

    Added full documentation to spritesheet_slicer, took card backs out of the zip folder and put it into spritesheet_files. Added deck_builder. Modified game.py to use the spritesheet in order to create the images and dictionary

--- game.py
+++ game.py
@@ -8,25 +8,14 @@ screen=pygame.display.set_mode((1000, 1000))
 spritesheet=pygame.image.load("spritesheet_files/playingCards.png")
 card_names=[["12S", "13S", "11S", "1S", "10S", "9S", "8S", "7S", "6S", "5S"],
             ["4S", "3S", "2S", "JOKER", "12H", "13H", "11H", "1H", "10H", "9H"],
-            ["8H", "7H", "6H", "5H"]]
-
-slicer.parse(spritesheet)
-
-spritesheet_size=spritesheet.get_size()
+            ["8H", "7H", "6H", "5H", "4H", "3H", "2C", "12D", "13D", "11D"],
+            ["1D", "10D", "9D", "8D", "7D", "6D", "5D", "4D", "3D", "2D"],
+            ["12C", "13C", "11C", "1C", "10C", "9C", "8C", "7C", "6C", "5C"],
+            ["4C", "3C", "2H"]]
 sprite_w=140
 sprite_h=190
-sprites=[]
 
-dict={"awesome":1, "peak":2, "sigma":3}
-for key in dict:
-    print(dict[key])
-
-for col in range(spritesheet_size[0]//sprite_w):
-    for row in range(spritesheet_size[1]//sprite_h):
-        surface=pygame.Surface((sprite_w, sprite_h), pygame.SRCALPHA)
-        surface.blit(spritesheet, (-sprite_w*(col), -sprite_h*(row)))
-        surface=pygame.transform.scale(surface, (sprite_w//2, sprite_h//2))
-        sprites.append(surface)
+sprites=slicer.parse(spritesheet, sprite_w, sprite_h, card_names)
 
 playing=True
 
@@ -36,8 +25,8 @@ while playing:
             pygame.quit()
             playing=False
     
-    for i in range(len(sprites)):
-        screen.blit(sprites[i], (15*i, 0))
+    for index, key in enumerate(sprites):
+        screen.blit(sprites[key], (15*index, 0))
 
     pygame.display.update()
     pygame.time.Clock().tick(60)
--- spritesheet_files/spritesheet_slicer.py
+++ spritesheet_files/spritesheet_slicer.py
@@ -20,11 +20,22 @@ def parse(spritesheet, sprite_w, sprite_h, sprite_names, vertical=True):
     spritesheet_size=spritesheet.get_size()
     #loop for running parsing a spritesheet that runs vertically
     if vertical:
+        #used to iterate over each column, uses the size of the spritesheet divided by the width of each sprite in order to run for each sprite
         for col in range(spritesheet_size[0]//sprite_w):
+            #used to iterate over each row, uses the size of the spritesheet divided by the width of each sprite in order to run for each sprite
             for row in range(spritesheet_size[1]//sprite_h):
+                #creates a surface with the size of the sprite that will store the image
                 surface=pygame.Surface((sprite_w, sprite_h), pygame.SRCALPHA)
+                #draws the spritesheet at a specific point on the surface in surface in order to get only a specific portion of the image, or the sprite
                 surface.blit(spritesheet, (-sprite_w*(col), -sprite_h*(row)))
+                #scales the image before adding it to the dictionary, comment out if you don't need to scale it
                 surface=pygame.transform.scale(surface, (sprite_w//2, sprite_h//2))
-                sprites[sprite_names[col][row]]=surface
+                #statement here in a try block in case you don't have enough key's in sprite_names, or if there is empty space in the sprite sheet were a sprite would maybe go
+                try:
+                    #adds the sprite to sprite list with the given name of the sprite, uses a 2D list, change if you so need
+                    sprites[sprite_names[col][row]]=surface
+                except IndexError:
+                    pass
     
+    #returns the newly created dictionary of sprite
     return sprites
