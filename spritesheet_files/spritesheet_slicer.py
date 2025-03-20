cd16e86e5dc92b3d0592a14ee2310bc6760d11dc
Jason <jason.t.richardii@gmail.com>
2025-03-20 18:54:21 -0400

    added non vertical to parser and documentation for it, (it's only a few small notes)

--- spritesheet_files/spritesheet_slicer.py
+++ spritesheet_files/spritesheet_slicer.py
@@ -24,7 +24,7 @@ def parse(spritesheet, sprite_w, sprite_h, sprite_names, vertical=True):
         for col in range(spritesheet_size[0]//sprite_w):
             #used to iterate over each row, uses the size of the spritesheet divided by the width of each sprite in order to run for each sprite
             for row in range(spritesheet_size[1]//sprite_h):
-                #creates a surface with the size of the sprite that will store the image
+                #creates a surface with the size of the sprite that will store the image, SCRALPHA gives it a transparent background
                 surface=pygame.Surface((sprite_w, sprite_h), pygame.SRCALPHA)
                 #draws the spritesheet at a specific point on the surface in surface in order to get only a specific portion of the image, or the sprite
                 surface.blit(spritesheet, (-sprite_w*(col), -sprite_h*(row)))
@@ -34,6 +34,21 @@ def parse(spritesheet, sprite_w, sprite_h, sprite_names, vertical=True):
                 try:
                     #adds the sprite to sprite list with the given name of the sprite, uses a 2D list, change if you so need
                     sprites[sprite_names[col][row]]=surface
+                except IndexError:
+                    pass
+    #if it's not vertical
+    #works identically, just some things are swapped
+    else:
+        #row and col are swapped, this changes the order that the sprites are iterated over
+        for row in range(spritesheet_size[1]//sprite_h):
+            for col in range(spritesheet_size[0]//sprite_w):
+                #identical
+                surface=pygame.Surface((sprite_w, sprite_h), pygame.SRCALPHA)
+                surface.blit(spritesheet, (-sprite_w*(col), -sprite_h*(row)))
+                surface=pygame.transform.scale(surface, (sprite_w//2, sprite_h//2))
+                try:
+                    #row and col are swapped, meaning the dictionary key is also different
+                    sprites[sprite_names[row][col]]=surface
                 except IndexError:
                     pass
     
